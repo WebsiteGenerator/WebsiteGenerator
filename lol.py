ee00057547cff9a379427266398b701367d17f7a
byZeroOfficial <76685126+byZeroOfficial@users.noreply.github.com>
2021-06-25 17:27:14 +0200

    v2.0
    - Social-Media-Icons
    - Custom Frames
    - Bug Fixes

--- lol.py
+++ lol.py
@@ -9,6 +9,9 @@ def createFolder(directory):
     except OSError:
         print ('Error: Creating directory. ' +  directory)
 
+createFolder('./website')
+createFolder('./resources/temp')
+
 # About Page:
 def about():
   location = input("Where do you live? | ")
@@ -34,17 +37,23 @@ def about():
   rs8 = rs8.read()
   rs9 = open("./resources/about/part9.txt", "r")
   rs9 = rs9.read()
-  createFolder('./about')
-  f = open("./about/index.html", "a+")
+  createFolder('./website/about')
+  f = open("./website/about/index.html", "a+")
   f.write(rs1 + name + rs2 + description + rs3 + previewimage + rs4 + location + rs5 + gender +  rs6 + birthday + rs7 + mail + rs8 + mail + rs9)
   print('Your About-Page was generated.')
   f.close()
 
+# Cards
 def card():
   cname = input('What is the name of the card? | ')
   cdescription = input("What is the description of the card? | ")
   curl = input("What is the URL of the card? | ")
   cimage = input("What is the image of the card? (You can enter a URL or a local path here). | ")
+  cframeyn = input("If you want to use a custom Frame enter your code - Else: Enter n or no | ")
+  if cframeyn == "n" or cframeyn == "no":
+    cframe = "card bg-gray-100 rounded-lg bg-gray-800 hover:shadow-xl p-5 content-around"
+  else:
+    cframe = cframeyn
   rs1 = open("./resources/cards/part1.txt", "r")
   rs1 = rs1.read()
   rs2 = open("./resources/cards/part2.txt", "r")
@@ -57,13 +66,31 @@ def card():
   rs5 = rs5.read()
   rs6 = open("./resources/cards/part6.txt", "r")
   rs6 = rs6.read()
-  ccard = rs1 + curl + rs2 + cimage + rs3 + cname + rs4 + cdescription + rs5 + curl + rs6
+  rsurl = open("./resources/cards/parturl.txt", "r")
+  rsurl = rsurl.read()
+  ccard = rs1 + cframe + rsurl + curl + rs2 + cimage + rs3 + cname + rs4 + cdescription + rs5 + curl + rs6
   ccards = '\n' + '\n' + ccard
   createFolder('./resources/temp')
   f = open("./resources/temp/cards.txt", "a+")
   f.write(ccards)
   f.close()
   addcard()
+
+def socialmedia():
+  sname = input("What is the name of the icon? | ")
+  simage = input("What is the image of the icon? (You can enter a URL or a local path here). | ")
+  surl = input("What is the URL of the icon? | ")
+  icon = "                <img src=\"" + simage + '" class="inline-block rounded-lg w-10 h-10" id=icon_' + sname + '" onclick="window.open(\'' + surl + '\')"> '
+  path = os.path.exists("./resources/temp/sci.txt")
+  if path == False:
+    f = open("./resources/temp/sci.txt", "a+")
+    f.write('        <div class="m-10" id="socials">\n            <center>' + "\n" + icon)
+    f.close()
+  else:
+    f = open("./resources/temp/sci.txt", "a")
+    f.write("\n" + icon)
+    f.close()
+  addscm()
 
 name = input("What is your name? | ")
 print(f'Thanks! {name} is a really nice name. :D')
@@ -83,6 +110,18 @@ def addcard():
   if addcard == "y" or addcard == "yes":
     card()
 addcard()
+
+def addscm():
+  addscme = input("Do you want to add a Social-Media Icon? (y or n) | ")
+  addscme = addscme.lower()
+  if addscme == "y" or addscme == "yes":
+    socialmedia()
+  else:
+    path = os.path.exists("./resources/temp/sci.txt")
+    if path == True:
+      f = open("./resources/temp/sci.txt", "a+")
+      f.write("\n            </center>\n    </footer>\n</html>")
+addscm()
 
 rs1 = open("./resources/index/part1.txt", "r")
 rs1 = rs1.read()
@@ -104,14 +143,28 @@ rs9 = open("./resources/index/part9.txt", "r")
 rs9 = rs9.read()
 rs10 = open("./resources/index/part10.txt", "r")
 rs10 = rs10.read()
-ccards = open("./resources/temp/cards.txt", "r")
-ccards = ccards.read()
-index = rs1 + domain + "\" content='" + description + rs2 + description + rs3 + previewimage + rs4 + name + rs5 + image + rs6 + name + rs7 + description + rs8 + image + rs9 + ccards + rs10
-f = open("index.html", "a+")
+
+path = os.path.exists("./resources/temp/cards.txt")
+if path == False:
+  ccards = ""
+elif path == True:
+  ccards = open("./resources/temp/cards.txt", "r")
+  ccards = ccards.read()
+
+path = os.path.exists("./resources/temp/sci.txt")
+if path == False:
+  socialmedias = ""
+elif path == True:
+  socialmedias = open("./resources/temp/sci.txt", "r")
+  socialmedias = socialmedias.read()
+index = rs1 + domain + "\" content='" + description + rs2 + description + rs3 + previewimage + rs4 + name + rs5 + image + rs6 + name + rs7 + description + rs8 + image + rs9 + ccards + rs10 + socialmedias + "\n	</footer>\n</html>"
+f = open("./website/index.html", "a+")
 f.write(index)
 f.close()
 if os.path.exists("./resources/temp/cards.txt"):
   os.remove("./resources/temp/cards.txt")
+if os.path.exists("./resources/temp/sci.txt"):
+  os.remove("./resources/temp/sci.txt")
 
 print('Thanks for using my Python-Script.')
 print('Done!')
