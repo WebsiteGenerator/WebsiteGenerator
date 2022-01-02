5b9fead787580393542ace93ba7a6647c6e929d6
byZero <byzero@zer0network.xyz>
2022-01-02 15:10:58 +0100

    Image Codes

--- src/main.py
+++ src/main.py
@@ -12,43 +12,17 @@ def createFolder(directory):
 createFolder('./website')
 createFolder('./resources/temp')
 
-# About Page:
-def about():
-  location = input("Where do you live? | ")
-  gender = input("What is your Gender? | ")
-  birthday = input("When is your Birthday? | ")
-  mail = input("What is your E-Mail-Adress? | ")
-  print("Your About-Page will be generated automatically within a few seconds.")
-  rs1 = open("./resources/about/part1.txt", "r")
-  rs1 = rs1.read()
-  rs2 = open("./resources/about/part2.txt", "r")
-  rs2 = rs2.read()
-  rs3 = open("./resources/about/part3.txt", "r")
-  rs3 = rs3.read()
-  rs4 = open("./resources/about/part4.txt", "r")
-  rs4 = rs4.read()
-  rs5 = open("./resources/about/part5.txt", "r")
-  rs5 = rs5.read()
-  rs6 = open("./resources/about/part6.txt", "r")
-  rs6 = rs6.read()
-  rs7 = open("./resources/about/part7.txt", "r")
-  rs7 = rs7.read()
-  rs8 = open("./resources/about/part8.txt", "r")
-  rs8 = rs8.read()
-  rs9 = open("./resources/about/part9.txt", "r")
-  rs9 = rs9.read()
-  createFolder('./website/about')
-  f = open("./website/about/index.html", "a+")
-  f.write(rs1 + name + rs2 + description + rs3 + previewimage + rs4 + location + rs5 + gender +  rs6 + birthday + rs7 + mail + rs8 + mail + rs9)
-  print('Your About-Page was generated.')
-  f.close()
-
 # Cards
 def card():
   cname = input('What is the name of the card? | ')
   cdescription = input("What is the description of the card? | ")
   curl = input("What is the URL of the card? | ")
-  cimage = input("What is the image of the card? (You can enter a URL or a local path here). | ")
+  print("\n")
+  print("Since version 3 there are automatic logos and icons for cards and social media icons if you specify the image code as URL for the images. You can find all image codes at https://by0.link/imagecodes.")
+  print("\n")
+  cimage = input("What is the image of the card? (You can enter a URL, an image code (https://by0.link/imagecodes) or a local path here). | ")
+  if not "/" in cimage:
+      cimage = "https://generat0r.byzero.dev/" + cimage + ".png"
   cframeyn = input("If you want to use a custom Frame enter your code - Else: Enter n or no | ")
   if cframeyn == "n" or cframeyn == "no":
     cframe = "card bg-gray-100 rounded-lg bg-gray-800 hover:shadow-xl p-5 content-around"
@@ -79,7 +53,12 @@ def card():
 
 def socialmedia():
   sname = input("What is the name of the icon? | ")
-  simage = input("What is the image of the icon? (You can enter a URL or a local path here). | ")
+  print("\n")
+  print("Since version 3 there are automatic logos and icons for cards and social media icons if you specify the image code as URL for the images. You can find all image codes at https://by0.link/imagecodes.")
+  print("\n")
+  simage = input("What is the image of the icon? (You can enter a URL, an image code (https://by0.link/imagecodes) or a local path here). | ")
+  if not "/" in simage:
+    simage = "https://generat0r.byzero.dev/" + simage + ".png"
   surl = input("What is the URL of the icon? | ")
   icon = "                <img src=\"" + simage + '" class="inline-block rounded-lg w-10 h-10" id=icon_' + sname + '" onclick="window.open(\'' + surl + '\')"> '
   path = os.path.exists("./resources/temp/sci.txt")
@@ -93,7 +72,8 @@ def socialmedia():
     f.close()
   addscm()
 
-name = input("What is your name? | ")
+
+name = input("What is the name for the website? (this could be your name or something else) | ")
 print(f'Thanks! {name} is a really nice name. :D')
 pronouns = input("What are your pronouns? (if you don't want to use pronouns, type no or n). | ")
 pronounslower = pronouns.lower()
@@ -103,11 +83,6 @@ description = input("What do you want to use as your description? | ")
 domain = input("What domain do you want to use? (Without https:// or http://) | ")
 image = input("What image do you want to use? (You can enter a URL or a local path here). | ")
 previewimage = input("What image do you want to use as a thumbnail? (Only URL) | ")
-
-aboutyorno = input("Do you want to add an About Me-Page? (y or n) | ")
-aboutyorno = aboutyorno.lower()
-if aboutyorno == "y" or aboutyorno == "yes":
-  about()
 
 def addcard():
   addcard = input("Do you want to add a card? (y or n) | ")
