42e797d756fc022942e07f0d52abf96e30e7a51c
byZeroOfficial <76685126+byZeroOfficial@users.noreply.github.com>
2022-01-28 17:33:12 +0100

    Added the possibility to download the resources automatically

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 import os
-import re
+import requests
+import zipfile
+import shutil
+
+# Download required resources
+downloadurl = 'https://raw.githubusercontent.com/WebsiteGenerator/resources/main/latest.zip'
+r = requests.get(downloadurl, allow_redirects=True)
+open('latest.zip', 'wb').write(r.content)
+with zipfile.ZipFile("latest.zip", 'r') as zip_ref:
+  zip_ref.extractall("./web/")
+# Remove the zip file
+os.remove('latest.zip')
 
 # Folder
 def createFolder(directory):
@@ -9,8 +20,8 @@ def createFolder(directory):
     except OSError:
         print ('Error: Creating directory. ' +  directory)
 
-createFolder('./website')
-createFolder('./resources/temp')
+createFolder('./web')
+createFolder('./web/wbsg_resources/temp')
 
 # Cards
 def card():
@@ -28,25 +39,25 @@ def card():
     cframe = "card bg-gray-100 rounded-lg bg-gray-800 hover:shadow-xl p-5 content-around"
   else:
     cframe = cframeyn
-  rs1 = open("./resources/cards/part1.txt", "r")
+  rs1 = open("./web/wbsg_resources/cards/part1.txt", "r")
   rs1 = rs1.read()
-  rs2 = open("./resources/cards/part2.txt", "r")
+  rs2 = open("./web/wbsg_resources/cards/part2.txt", "r")
   rs2 = rs2.read()
-  rs3 = open("./resources/cards/part3.txt", "r")
+  rs3 = open("./web/wbsg_resources/cards/part3.txt", "r")
   rs3 = rs3.read()
-  rs4 = open("./resources/cards/part4.txt", "r")
+  rs4 = open("./web/wbsg_resources/cards/part4.txt", "r")
   rs4 = rs4.read()
-  rs5 = open("./resources/cards/part5.txt", "r")
+  rs5 = open("./web/wbsg_resources/cards/part5.txt", "r")
   rs5 = rs5.read()
-  rs6 = open("./resources/cards/part6.txt", "r")
+  rs6 = open("./web/wbsg_resources/cards/part6.txt", "r")
   rs6 = rs6.read()
-  rsurl = open("./resources/cards/parturl.txt", "r")
+  rsurl = open("./web/wbsg_resources/cards/parturl.txt", "r")
   rsurl = rsurl.read()
   cpurl = curl.replace('https://','')
   ccard = rs1 + cframe + rsurl + curl + rs2 + cimage + rs3 + cname + rs4 + cdescription + rs5 + cpurl + rs6
   ccards = '\n' + '\n' + ccard
-  createFolder('./resources/temp')
-  f = open("./resources/temp/cards.txt", "a+")
+  createFolder('./web/wbsg_resources/temp')
+  f = open("./web/wbsg_resources/temp/cards.txt", "a+")
   f.write(ccards)
   f.close()
   addcard()
@@ -61,13 +72,13 @@ def socialmedia():
     simage = "https://generat0r.byzero.dev/" + simage + ".png"
   surl = input("What is the URL of the icon? | ")
   icon = "                <img src=\"" + simage + '" class="inline-block rounded-lg w-10 h-10" id=icon_' + sname + '" onclick="window.open(\'' + surl + '\')"> '
-  path = os.path.exists("./resources/temp/sci.txt")
+  path = os.path.exists("./web/wbsg_resources/temp/sci.txt")
   if path == False:
-    f = open("./resources/temp/sci.txt", "a+")
+    f = open("./web/wbsg_resources/temp/sci.txt", "a+")
     f.write('        <div class="m-10" id="socials">\n            <center>' + "\n" + icon)
     f.close()
   else:
-    f = open("./resources/temp/sci.txt", "a")
+    f = open("./web/wbsg_resources/temp/sci.txt", "a")
     f.write("\n" + icon)
     f.close()
   addscm()
@@ -97,60 +108,62 @@ def addscm():
   if addscme == "y" or addscme == "yes":
     socialmedia()
   else:
-    path = os.path.exists("./resources/temp/sci.txt")
+    path = os.path.exists("./web/wbsg_resources/temp/sci.txt")
     if path == True:
-      f = open("./resources/temp/sci.txt", "a+")
+      f = open("./web/wbsg_resources/temp/sci.txt", "a+")
       f.write("\n            </center>\n    </footer>\n</html>")
 addscm()
 
-rs1 = open("./resources/index/part1.txt", "r")
+rs1 = open("./web/wbsg_resources/index/part1.txt", "r")
 rs1 = rs1.read()
-rs2 = open("./resources/index/part2.txt", "r")
+rs2 = open("./web/wbsg_resources/index/part2.txt", "r")
 rs2 = rs2.read()
-rs3 = open("./resources/index/part3.txt", "r")
+rs3 = open("./web/wbsg_resources/index/part3.txt", "r")
 rs3 = rs3.read()
-rs4 = open("./resources/index/part4.txt", "r")
+rs4 = open("./web/wbsg_resources/index/part4.txt", "r")
 rs4 = rs4.read()
-rs5 = open("./resources/index/part5.txt", "r")
+rs5 = open("./web/wbsg_resources/index/part5.txt", "r")
 rs5 = rs5.read()
-rs6 = open("./resources/index/part6.txt", "r")
+rs6 = open("./web/wbsg_resources/index/part6.txt", "r")
 rs6 = rs6.read()
-rs7 = open("./resources/index/part7.txt", "r")
+rs7 = open("./web/wbsg_resources/index/part7.txt", "r")
 rs7 = rs7.read()
-rs8 = open("./resources/index/part8.txt", "r")
+rs8 = open("./web/wbsg_resources/index/part8.txt", "r")
 rs8 = rs8.read()
-rs9 = open("./resources/index/part9.txt", "r")
+rs9 = open("./web/wbsg_resources/index/part9.txt", "r")
 rs9 = rs9.read()
-rs10 = open("./resources/index/part10.txt", "r")
+rs10 = open("./web/wbsg_resources/index/part10.txt", "r")
 rs10 = rs10.read()
-partpronouns = open("./resources/index/partpronouns.txt", "r")
+partpronouns = open("./web/wbsg_resources/index/partpronouns.txt", "r")
 partpronouns = partpronouns.read()
-css = open("./resources/index/css.txt", "r")
+css = open("./web/wbsg_resources/index/css.txt", "r")
 css = css.read()
 
-path = os.path.exists("./resources/temp/cards.txt")
+path = os.path.exists("./web/wbsg_resources/temp/cards.txt")
 if path == False:
   ccards = ""
 elif path == True:
-  ccards = open("./resources/temp/cards.txt", "r")
+  ccards = open("./web/wbsg_resources/temp/cards.txt", "r")
   ccards = ccards.read()
 
-path = os.path.exists("./resources/temp/sci.txt")
+path = os.path.exists("./web/wbsg_resources/temp/sci.txt")
 if path == False:
   socialmedias = ""
 elif path == True:
-  socialmedias = open("./resources/temp/sci.txt", "r")
+  socialmedias = open("./web/wbsg_resources/temp/sci.txt", "r")
   socialmedias = socialmedias.read()
 index = rs1 + domain + "\" content='" + name + rs2 + description + rs3 + previewimage + rs4 + name + rs5 + image + rs6 + name + rs7 + description + rs8 + pronouns + partpronouns + image + rs9 + ccards + rs10 + socialmedias + "\n	</footer>\n</html>"
-f = open("./website/index.html", "a+")
+f = open("./web/index.html", "a+")
 f.write(index)
 f.close()
-if os.path.exists("./resources/temp/cards.txt"):
-  os.remove("./resources/temp/cards.txt")
-if os.path.exists("./resources/temp/sci.txt"):
-  os.remove("./resources/temp/sci.txt")
-filecss = open("./website/style.css", "a+")
+if os.path.exists("./web/wbsg_resources/temp/cards.txt"):
+  os.remove("./web/wbsg_resources/temp/cards.txt")
+if os.path.exists("./web/wbsg_resources/temp/sci.txt"):
+  os.remove("./web/wbsg_resources/temp/sci.txt")
+filecss = open("./web/style.css", "a+")
 filecss.write(css)
+
+shutil.rmtree("./web/wbsg_resources/")
 
 print('Thanks for using my Python-Script.')
 print('Done!')
